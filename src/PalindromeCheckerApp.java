35ce1afda0f8815a03d9b00772a60d546ec56175
Ravikiran-it <ravikiranmeenavilli2006@gmail.com>
2026-03-04 22:35:10 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,22 @@
+/**
+ * =====================================================================
+ * MAIN CLASS - PalindromeCheckerApp
+ * =====================================================================
+ * Use Case 1: Application Entry & Welcome Message
+ * * Description:
+ * This class represents the entry point of the Palindrome Checker Management System.
+ * @version 1.0
+ */
 public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point.
+     * This is the first method executed by the JVM when the program starts.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
 }
